c97434084e16895cad6a6ae9d07682e7fc7c584b
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-07-12 22:22:02 +0530

    Add constant.dart

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+//Padding & Margin
+import 'package:flutter/material.dart';
+
+const kDefaultPadding = 20.0;
+
+//Colors
+const kPrimaryButtonColor = Color(0xFF2EA043);
+const kSecondaryButtonColor = Color(0xFF1F6FEB);
