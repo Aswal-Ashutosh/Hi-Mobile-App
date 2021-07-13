a41e2d79d0e4d6e0a590f179420bf7056778f0fe
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-07-13 17:10:49 +0530

    Minor changes in Welcome Screen

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,29 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:hi/constants/constants.dart';
 import 'package:hi/custom_widget/buttons/primary_button.dart';
+import 'package:hi/screens/sign_up_screen.dart';
 
 class SignInScreen extends StatelessWidget {
-  //const SignInScreen({ Key? key }) : super(key: key);
   static const id = 'sign_in_screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Text(
-                'Sign In',
-                style: TextStyle(
-                  fontSize: 40,
-                ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(child: Text('Sign In', style: TextStyle(fontSize: 40))),
+              SizedBox(height: kDefaultPadding * 2),
+              SignInForm(),
+              SizedBox(height: kDefaultPadding),
+              Row(
+                children: [
+                  Expanded(child: Divider(height: 1.5, color: Colors.black)),
+                  Text('OR', style: TextStyle(fontWeight: FontWeight.bold)),
+                  Expanded(child: Divider(height: 1.5, color: Colors.black)),
+                ],
               ),
-            ),
-            SizedBox(height: kDefaultPadding * 2),
-            SignInForm()
-          ],
+              SizedBox(height: kDefaultPadding),
+              CircleAvatar(
+                child: Image.asset('assets/google.png'),
+                backgroundColor: Colors.grey,
+              ),
+              SizedBox(height: kDefaultPadding),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don\'t have an account?'),
+                  SizedBox(width: kDefaultPadding / 5.0),
+                  GestureDetector(
+                    onTap: () =>
+                        Navigator.popAndPushNamed(context, SignUpScreen.id),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -38,57 +63,55 @@ class SignInForm extends StatefulWidget {
 class _SignInFormState extends State<SignInForm> {
   bool obscureText = true;
   final borderRadius = OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(40)),
-      borderSide: BorderSide(color: Colors.green));
+    borderRadius: BorderRadius.all(Radius.circular(40)),
+    borderSide: BorderSide(color: kPrimaryButtonColor),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color(0x332EA043),
-                hintText: 'Your Email',
-                enabledBorder: borderRadius,
-                focusedBorder: borderRadius,
-                errorBorder: borderRadius,
-                focusedErrorBorder: borderRadius,
-                prefixIcon: Icon(Icons.person),
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color(0x332EA043),
+              hintText: 'Your Email',
+              enabledBorder: borderRadius,
+              focusedBorder: borderRadius,
+              errorBorder: borderRadius,
+              focusedErrorBorder: borderRadius,
+              prefixIcon: Icon(Icons.mail),
             ),
-            SizedBox(height: kDefaultPadding),
-            TextFormField(
-              obscureText: obscureText,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color(0x332EA043),
-                hintText: 'Password',
-                enabledBorder: borderRadius,
-                focusedBorder: borderRadius,
-                errorBorder: borderRadius,
-                focusedErrorBorder: borderRadius,
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: GestureDetector(
-                    child: Icon(
-                        obscureText ? Icons.visibility_off : Icons.visibility),
-                    onTap: () {
-                      setState(() {
-                        obscureText = !obscureText;
-                      });
-                    }),
-              ),
+          ),
+          SizedBox(height: kDefaultPadding),
+          TextFormField(
+            obscureText: obscureText,
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color(0x332EA043),
+              hintText: 'Password',
+              enabledBorder: borderRadius,
+              focusedBorder: borderRadius,
+              errorBorder: borderRadius,
+              focusedErrorBorder: borderRadius,
+              prefixIcon: Icon(Icons.lock),
+              suffixIcon: GestureDetector(
+                  child: Icon(
+                      obscureText ? Icons.visibility_off : Icons.visibility),
+                  onTap: () {
+                    setState(() {
+                      obscureText = !obscureText;
+                    });
+                  }),
             ),
-            SizedBox(height: kDefaultPadding),
-            PrimaryButton(displayText: 'Sign In', onPressed: () {}),
-          ],
-        ),
+          ),
+          SizedBox(height: kDefaultPadding),
+          PrimaryButton(displayText: 'Sign In', onPressed: () {}),
+        ],
       ),
     );
   }
