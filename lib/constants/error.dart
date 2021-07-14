e9dffb9cf228b28ce460f2d9449de9be48890b4c
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-07-14 22:26:11 +0530

    Add error.dart

---
+++ lib/constants/error.dart
@@ -0,0 +1,4 @@
+const String invalid_email = 'Invalid email.';
+const String user_not_found = 'There is no user record corresponding to this email.';
+const String wrong_password = 'The password is invalid.';
+const String email_already_in_use = 'The email address is already in use by another account';
