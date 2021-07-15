05ef0e8c16199a036cde4d2912c0ecec45d370c2
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-07-15 21:07:41 +0530

    Add too_many_mail_request error message

--- lib/constants/error.dart
+++ lib/constants/error.dart
@@ -2,3 +2,4 @@ const String invalid_email = 'Invalid email.';
 const String user_not_found = 'There is no user record corresponding to this email.';
 const String wrong_password = 'The password is invalid.';
 const String email_already_in_use = 'The email address is already in use by another account';
+const String too_many_mail_request = 'Unable to send verification mail. Request again by clicking the Resend Button.';
