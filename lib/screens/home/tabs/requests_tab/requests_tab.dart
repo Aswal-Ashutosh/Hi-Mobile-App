4aace8feae5fd225e63b26a90271de91d12c3f6e
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-07-22 16:57:13 +0530

    Fetch requests from database and display Friend Request Card for each request

--- lib/screens/home/tabs/requests_tab/requests_tab.dart
+++ lib/screens/home/tabs/requests_tab/requests_tab.dart
@@ -1,10 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hi/custom_widget/friend_request_card.dart';
+import 'package:hi/services/firebase_service.dart';
 
 class RequestsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseService.currentUserStreamToFirendRequests,
+        builder: (conext, snapshot) {
+          List<FriendRequestCard> friendRequests = [];
+          if(snapshot.hasData && snapshot.data!.docs.isNotEmpty){
+            final requests = snapshot.data?.docs;
+            if(requests != null){
+              for(final request in requests){
+                final senderEmail = request['sender_email'];
+                final date = request['date'];
+                final time = request['time'];
+                friendRequests.add(
+                  FriendRequestCard(senderEmail: senderEmail, date: date, time: time)
+                );
+              }
+            }
+          }
+          return ListView(
+            children: friendRequests,
+          );
+        },
+      ),
     );
   }
 }
