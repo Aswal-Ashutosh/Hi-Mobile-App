dfc1a7cb606551536f70f5b4c3d3d7bafc0b935a
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-07-24 17:32:41 +0530

    RoundIconButton will now take radius as a parameter

--- lib/custom_widget/buttons/round_icon_button.dart
+++ lib/custom_widget/buttons/round_icon_button.dart
@@ -5,13 +5,18 @@ class RoundIconButton extends StatelessWidget {
   final IconData icon;
   final onPressed;
   final Color color;
+  final radius;
 
-  const RoundIconButton({required this.icon, required this.onPressed, this.color = kPrimaryColor});
+  const RoundIconButton({required this.icon, required this.onPressed, this.color = kPrimaryColor, this.radius = kDefualtBorderRadius * 2});
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
       shape: CircleBorder(),
+       constraints: BoxConstraints.tightFor(
+        width: radius,
+        height: radius,
+      ),
       fillColor: color,
       onPressed: onPressed,
       elevation: 5.0,
