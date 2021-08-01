28b06fab2859991be182f6e161cdf70afb79bde2
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-08-01 23:29:04 +0530

    Include Group Chat Card

--- lib/screens/home/tabs/chat_tab/chat_tab.dart
+++ lib/screens/home/tabs/chat_tab/chat_tab.dart
@@ -4,9 +4,15 @@ import 'package:hi/constants/constants.dart';
 import 'package:hi/constants/firestore_costants.dart';
 import 'package:hi/screens/group/group_chat_selection_screen.dart';
 import 'package:hi/screens/home/tabs/chat_tab/components/chat_card_one_to_one.dart';
+import 'package:hi/screens/home/tabs/chat_tab/components/group_chat_card.dart';
 import 'package:hi/services/firebase_service.dart';
 
-class ChatTab extends StatelessWidget {
+class ChatTab extends StatefulWidget {
+  @override
+  _ChatTabState createState() => _ChatTabState();
+}
+
+class _ChatTabState extends State<ChatTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,16 +27,26 @@ class ChatTab extends StatelessWidget {
                 if (snapshots.hasData && snapshots.data!.docs.isNotEmpty) {
                   final chats = snapshots.data?.docs;
                   chats?.forEach((element) {
-                    late String friendEmail;
-                    for (final email in element[ChatDBDocumentField.MEMBERS]) {
-                      if (email != FirebaseService.currentUserEmail) {
-                        friendEmail = email;
+                    if (element[ChatDBDocumentField.TYPE] ==
+                        ChatType.ONE_TO_ONE) {
+                      late String friendEmail;
+                      for (final email
+                          in element[ChatDBDocumentField.MEMBERS]) {
+                        if (email != FirebaseService.currentUserEmail) {
+                          friendEmail = email;
+                        }
                       }
+                      chatCards.add(ChatCardOneToOne(
+                        roomId: element[ChatDBDocumentField.ROOM_ID],
+                        friendEmail: friendEmail,
+                      ));
+                    } else {
+                      chatCards.add(
+                        GroupChatCard(
+                          roomId: element[GroupDBDocumentField.ROOM_ID],
+                        ),
+                      );
                     }
-                    chatCards.add(ChatCardOneToOne(
-                      roomId: element[ChatDBDocumentField.ROOM_ID],
-                      friendEmail: friendEmail,
-                    ));
                   });
                 }
                 return ListView(
@@ -44,8 +60,15 @@ class ChatTab extends StatelessWidget {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, GroupChatSelectionScreen.id);
+        onPressed: () async {
+          final result =
+              await Navigator.pushNamed(context, GroupChatSelectionScreen.id);
+          if (result == true) {
+            setState(() {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Group Created Successfully.')));
+            });
+          }
         },
         backgroundColor: kPrimaryColor,
         child: Icon(Icons.group_add),
