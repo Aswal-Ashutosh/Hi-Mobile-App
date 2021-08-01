b5d10d00fb55c0db50d47332665303f81d5db52c
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-08-01 23:24:35 +0530

    Add Group Setup Screen

---
+++ lib/screens/group/group_setup_screen.dart
@@ -0,0 +1,169 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:hi/constants/constants.dart';
+import 'package:hi/custom_widget/buttons/primary_button.dart';
+import 'package:hi/custom_widget/buttons/round_icon_button.dart';
+import 'package:hi/custom_widget/progressHud/progress_hud.dart';
+import 'package:hi/services/firebase_service.dart';
+import 'package:image_picker/image_picker.dart';
+
+class GroupSetupScreen extends StatefulWidget {
+  final List<String> _selectedUsers;
+  const GroupSetupScreen({required final List<String> selectedUsers})
+      : _selectedUsers = selectedUsers;
+
+  @override
+  _GroupSetupScreenState createState() => _GroupSetupScreenState();
+}
+
+class _GroupSetupScreenState extends State<GroupSetupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameTextController = TextEditingController();
+  final _aboutTextController =
+      TextEditingController(text: 'Let\'s talk together🖐.');
+
+  final _validator = (String? value) =>
+      value!.trim().isEmpty ? "This field can't be empty." : null;
+
+  final borderRadius = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(kDefualtBorderRadius * 2)),
+    borderSide: BorderSide(color: kPrimaryColor),
+  );
+
+  File? profileImage;
+
+  bool loading = false;
+
+  @override
+  void dispose() {
+    _nameTextController.dispose();
+    _aboutTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProgressHUD(
+      showIndicator: loading,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar:
+            AppBar(title: Text('Setup Group'), backgroundColor: kPrimaryColor),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    profileImage == null
+                        ? CircleAvatar(
+                            child: Icon(
+                              Icons.group,
+                              color: Colors.grey,
+                              size: kDefualtBorderRadius * 3,
+                            ),
+                            backgroundColor: Colors.blueGrey,
+                            radius: kDefualtBorderRadius * 3,
+                          )
+                        : CircleAvatar(
+                            backgroundImage: FileImage(profileImage as File),
+                            backgroundColor: Colors.blueGrey,
+                            radius: kDefualtBorderRadius * 3,
+                          ),
+                    Positioned(
+                      child: RoundIconButton(
+                        icon: Icons.edit,
+                        onPressed: () async {
+                          final ImagePicker imagePicker = ImagePicker();
+                          XFile? image = await imagePicker.pickImage(
+                              source: ImageSource.gallery);
+                          if (image != null) {
+                            setState(() {
+                              profileImage = File(image.path);
+                            });
+                          }
+                        },
+                        color: Colors.blueGrey,
+                      ),
+                      right: 0,
+                      bottom: 0,
+                      width: kDefualtBorderRadius * 2.0,
+                    )
+                  ],
+                ),
+                SizedBox(height: kDefaultPadding),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFormField(
+                        controller: _nameTextController,
+                        validator: _validator,
+                        textAlign: TextAlign.center,
+                        maxLength: 40,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0x112EA043),
+                          labelText: 'Group Name',
+                          enabledBorder: borderRadius,
+                          focusedBorder: borderRadius,
+                          errorBorder: borderRadius,
+                          focusedErrorBorder: borderRadius,
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                      ),
+                      SizedBox(height: kDefaultPadding),
+                      TextFormField(
+                        textInputAction: TextInputAction.done,
+                        controller: _aboutTextController,
+                        validator: _validator,
+                        textAlign: TextAlign.left,
+                        minLines: 1,
+                        maxLines: 3,
+                        maxLength: 120,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0x112EA043),
+                          labelText: 'About',
+                          enabledBorder: borderRadius,
+                          focusedBorder: borderRadius,
+                          errorBorder: borderRadius,
+                          focusedErrorBorder: borderRadius,
+                          prefixIcon: Icon(Icons.menu_book),
+                        ),
+                      ),
+                      SizedBox(height: kDefaultPadding),
+                      PrimaryButton(
+                        displayText: 'Save',
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() {
+                              loading = true;
+                            });
+                            await FirebaseService.createNewGroup(
+                                    members: widget._selectedUsers,
+                                    groupName: _nameTextController.text.trim(),
+                                    aboutGroup: _aboutTextController.text.trim(),
+                                    groupImage: profileImage)
+                                .then(
+                              (value) => Navigator.pop(context, true),
+                            );
+                            setState(() {
+                              loading = false;
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
