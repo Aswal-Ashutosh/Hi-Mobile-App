a70477715c539c13201169f5d9c2a1fa2afd99f7
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-08-09 19:13:35 +0530

    Loading Screen Optimization

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hi/custom_widget/progressHud/progress_hud.dart';
 import 'package:hi/screens/home/home_screen.dart';
 import 'package:hi/screens/welcome_screen.dart';
 import 'package:hi/services/firebase_service.dart';
@@ -13,7 +12,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  bool isLoading = true;
   @override
   void initState() {
     super.initState();
@@ -22,29 +20,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void loadStuff() async{
     await Firebase.initializeApp();
-    if(FirebaseAuth.instance.currentUser != null && await FirebaseService.userHasSetupProfile){
-      setState(() {
-        isLoading = false;
-      });
+    if(FirebaseAuth.instance.currentUser != null && await FirebaseService.userHasSetupProfile)
       Navigator.popAndPushNamed(context, HomeScreen.id);
-    }
-    else{
-      setState(() {
-        isLoading = false;
-      });
+    else
       Navigator.popAndPushNamed(context, WelcomeScreen.id);
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return ProgressHUD(
-      showIndicator: isLoading,
-      child: Scaffold(
-        body: SafeArea(
-          child: Container(
-            child: Center(child: Text('Loading', style: TextStyle(letterSpacing: 1.5))),
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Center(child: CircularProgressIndicator()),
         ),
       ),
     );
