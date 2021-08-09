64a12dc27be2055d24389f7421dedae866ba2293
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-08-09 17:31:17 +0530

    Disabling messaging and online indicator for the chats with friends which are removed from friends by the user

--- lib/screens/chat/one_to_one/chat_room.dart
+++ lib/screens/chat/one_to_one/chat_room.dart
@@ -8,7 +8,6 @@ import 'package:hi/custom_widget/stream_builders/text_stream_builder.dart';
 import 'package:hi/screens/chat/one_to_one/components/image_message.dart';
 import 'package:hi/screens/chat/one_to_one/components/message_text_field.dart';
 import 'package:hi/screens/chat/one_to_one/components/text_message.dart';
-import 'package:hi/screens/profile_view/user_profile_view_screen.dart';
 import 'package:hi/services/encryption_service.dart';
 import 'package:hi/services/firebase_service.dart';
 
@@ -21,39 +20,77 @@ class ChatRoom extends StatelessWidget {
         _friendEmail = friendEamil;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: InkWell(
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => UserProfileScreen(userEmail: _friendEmail),
-            ),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(kDefaultPadding / 4.0),
-                child: CircularProfilePicture(
-                  email: _friendEmail,
-                  radius: kDefualtBorderRadius,
+    return FutureBuilder<bool>(
+      future: FirebaseService.isFriend(email: _friendEmail),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError || snapshot.data == null)
+            return Center(
+                child: Scaffold(
+              body: SafeArea(
+                child: Center(
+                  child: Text(
+                    'Something went wrong!',
+                    style: TextStyle(color: Colors.grey, fontSize: 15),
+                  ),
                 ),
               ),
-              SizedBox(width: kDefaultPadding / 4.0),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  TextStreamBuilder(
-                    stream: FirebaseService.getStreamToUserData(
-                        email: _friendEmail),
-                    key: UserDocumentField.DISPLAY_NAME,
-                  ),
-                  OnlineIndicatorText(email: _friendEmail)
-                ],
+            ));
+          else
+            return Body(
+                roomId: _roomId,
+                friendEmail: _friendEmail,
+                isFriend: snapshot.data as bool);
+        } else {
+          return Scaffold(body: SafeArea(child: Center(child: CircularProgressIndicator())));
+        }
+      },
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+    required String friendEmail,
+    required String roomId,
+    required bool isFriend,
+  })  : _friendEmail = friendEmail,
+        _roomId = roomId,
+        _isFriend = isFriend,
+        super(key: key);
+
+  final String _friendEmail;
+  final String _roomId;
+  final bool _isFriend;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(kDefaultPadding / 4.0),
+              child: CircularProfilePicture(
+                email: _friendEmail,
+                radius: kDefualtBorderRadius,
               ),
-            ],
-          ),
+            ),
+            SizedBox(width: kDefaultPadding / 4.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextStreamBuilder(
+                  stream:
+                      FirebaseService.getStreamToUserData(email: _friendEmail),
+                  key: UserDocumentField.DISPLAY_NAME,
+                ),
+                if(_isFriend)
+                  OnlineIndicatorText(email: _friendEmail)
+              ],
+            ),
+          ],
         ),
         backgroundColor: kPrimaryColor,
       ),
@@ -115,16 +152,30 @@ class ChatRoom extends StatelessWidget {
             Positioned(
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                child: MessageTextField(
+                child: _isFriend ? MessageTextField(
                   roomId: _roomId,
                   friendEmail: _friendEmail,
-                ),
+                ) : NoLongerFriends(),
               ),
               bottom: 0,
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class NoLongerFriends extends StatelessWidget {
+  const NoLongerFriends({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(kDefaultPadding),
+      child: Center(child: Text('You are no logner friends.', style: TextStyle(color: Colors.grey),)),
     );
   }
 }
