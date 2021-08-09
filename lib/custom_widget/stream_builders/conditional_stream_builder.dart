e909cee4df94576105e4ec82ab21320480b344e5
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2021-08-09 21:17:48 +0530

    Add ConditionalStreamBuilder

---
+++ lib/custom_widget/stream_builders/conditional_stream_builder.dart
@@ -0,0 +1,32 @@
+/*
+CONDTIONAL STREAM BUILDER WILL STREAM TO DOCUMENT AND TWO CHILD AS INPUT AND DEPENDING UPON WEATHER
+DOCUMENET EXIST IT WILL BUILD THE EITHER OF THE ONE CHILD
+*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ConditionalStreamBuilder extends StatelessWidget {
+  const ConditionalStreamBuilder(
+      {required final Stream<DocumentSnapshot<Object?>> stream,
+      required final Widget childIfExist,
+      required final Widget childIfDoNotExist})
+      : _stream = stream,
+        _childIfExist = childIfExist,
+        _childIfDoNotExist = childIfDoNotExist;
+
+  final Stream<DocumentSnapshot<Object?>> _stream;
+  final Widget _childIfExist;
+  final Widget _childIfDoNotExist;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+      stream: _stream,
+      builder: (context, snapshot) {
+        bool exist =
+            snapshot.hasData && snapshot.data != null && snapshot.data!.exists;
+        return exist ? _childIfExist : _childIfDoNotExist;
+      },
+    );
+  }
+}
